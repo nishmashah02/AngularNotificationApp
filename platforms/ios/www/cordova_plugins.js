fb9b6aa6ff1e519c0096b6f9ed8f75c80945fac2
nishmashah02 <43072951+nishmashah02@users.noreply.github.com>
2018-10-30 23:43:56 +0530

    Added IOS platform to cordova project. Deleted Cordova Library so as to upload project on GitHub

---
+++ platforms/ios/www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+	"file":"plugins/com.phonegap.plugins.PushPlugin/www/pushNotification.js",
+	"id": "com.phonegap.plugins.pushPlugin.PushNotification",
+	"pluginId":"com.phonegap.plugins.PushPlugin",
+	"clobbers": [
+"PushNotification"
+	]
+},
+{
+  "cordova-plugin-whitelist": "1.3.3"
+};
+// BOTTOM OF METADATA
+});
